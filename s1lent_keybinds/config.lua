16feccbe327da9ba682c8b1b82da6638e1a5e3c8
apurvaroleplay <70094093+apurvaroleplay@users.noreply.github.com>
2020-08-25 22:20:26 +0800

    keybind
    keybind

---
+++ s1lent_keybinds/config.lua
@@ -0,0 +1,42 @@
+Config = {}
+
+Config.keybinds = {
+    {
+        category='General',
+        keys={
+            {description='Radial Menu', key='F1'},
+            {description='Inventory', key='F2'},
+            {description='Animation Menu ', key='F3'},
+            {description='Job Menu', key='F6'},
+            {description='Console', key='F8'},
+            {description='Scoreboard', key='F10'},
+            {description='Angkat Tangan', key='X'},
+            {description='Menunjuk', key='B'},
+            {description='Ganti View Kamera', key='V'},
+            {description='Refresh Karakter Saat Pingsan', key='H'},
+            {description='Mengganti Mode Suara', key='Z'},
+            {description='Berbicara Di Radio/Telp', key='CAPS LOCK'},
+        },
+    },
+    {
+        category='Vehicle',
+        keys={
+        	{description='Sirene', key='Q'},
+        	{description='Sirene Sound', key='E'},
+        	{description='Sirene Sound 2', key='Arrow Up'},
+        	{description='Horn', key='E'},
+            {description='Hazards', key='Arrow Down'},
+            {description='Keluar/Masuk Vehicle', key='F'},
+            {description='Lock/Unlock Vehice', key='U'},
+            {description='Bagasi/Trunk', key='L'},
+            {description='Seatbelt', key='B'},
+            {description='Membatasi Kecepatan', key='x'},
+        },
+    },
+    -- {
+    --     category='',
+    --     keys={
+    --         {description='', key=''},
+    --     },
+    -- },
+}
